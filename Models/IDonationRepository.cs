f54914d19f2e46913eb9e282fb3b5f96cfc7531f
Kalvin Wettstein <kalvinwettstein@gmail.com>
2022-03-02 10:35:01 -0700

    Mission 9 videos part 2

---
+++ Models/IDonationRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+namespace WaterProject.Models
+{
+    public interface IDonationRepository
+    {
+        IQueryable<Donation> Donations { get; }
+
+        public void SaveDonation(Donation donation);
+    }
+}
